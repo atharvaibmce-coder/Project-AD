2f6a4f79a7bbce4162c5ffbc1385af3c56c0eb5d
athu2773 <your.email@example.com>
2026-08-13 17:30:47 +0530

    updated the port security and script path

--- Backend/index.js
+++ Backend/index.js
@@ -23,6 +23,8 @@ app.use("/api/auth", userRouter);
 app.use("/api/todos", todoRouter);
 
 
-app.listen(3000, ()=>{
-    console.log(`server running on port 3000`);
-})
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
